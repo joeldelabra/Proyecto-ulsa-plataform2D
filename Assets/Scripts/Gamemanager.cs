2763ea59002084350559fc93965a9c406895a005
Joel De Labra <JoelDeLabra@gmail.com>
2020-02-16 19:40:08 -0700

    Primera version
    segunda

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public static Gamemanager instance;
+    [SerializeField]
+    Score score;
+
+    public Score Score { get => score; }
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
