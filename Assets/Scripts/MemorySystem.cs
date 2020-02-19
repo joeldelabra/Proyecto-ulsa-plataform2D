9443ede8d1d1673558353a2749080354b7808a41
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2020-02-19 07:53:14 -0700

    Save data

---
+++ Assets/Scripts/MemorySystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Platform2DUtils.MemorySystem
+{
+   public class MemorySystem
+   {
+       public static void SaveData(GameData GameData) 
+       {
+           string path = $"{Application.persistentDataPath}/myGame.data";
+           BinaryFormatter bf = new BinaryFormatter();
+           FileStream file = File.Create (path);
+           string json = JsonUtility.ToJson(gameData);
+           bf.Serialize(file,json);
+           file.Close();
+            Debug.log(path);
+       }
+
+
+
+   }
+
+}
